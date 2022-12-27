1540432ee5be877104c775d34d06c754635bb3f8
hau-td-reeCorp <hau.td@reecorp.com>
2022-12-27 16:01:19 +0700

    added MUI

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,42 +1,173 @@
-import React from "react";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+// import React from "react";
+// import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+// import "./styles.scss";
+
+// const Login = () => {
+//   return (
+//     <div fluid className="login-page d-flex justify-content-center align-items-center  img-fluid">
+//       <div className="login-container">
+//         <div className="logo">
+//           <img
+//             src={require("../../assets/logo/logo.svg").default}
+//             alt="REE logo"
+//           />
+//         </div>
+
+//         <Form className="form">
+//           <FormGroup>
+//             <Label for="exampleEmail">Username</Label>
+//             <Input
+//               type="email"
+//               name="email"
+//               id="exampleEmail"
+//               placeholder="example@example.com"
+//             />
+//           </FormGroup>
+//           <FormGroup>
+//             <Label for="examplePassword">Password</Label>
+//             <Input
+//               type="password"
+//               name="password"
+//               id="examplePassword"
+//               placeholder="********"
+//             />
+//           </FormGroup>
+//           <Button className="w-100">Submit</Button>
+//         </Form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
+
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./styles.scss";
 
-const Login = () => {
+function Copyright(props) {
   return (
-    <div fluid className="login-page d-flex justify-content-center align-items-center  img-fluid">
-      <div className="login-container">
-        <div className="logo">
-          <img
-            src={require("../../assets/logo/logo.svg").default}
-            alt="REE logo"
-          />
-        </div>
-
-        <Form className="form">
-          <FormGroup>
-            <Label for="exampleEmail">Username</Label>
-            <Input
-              type="email"
-              name="email"
-              id="exampleEmail"
-              placeholder="example@example.com"
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label for="examplePassword">Password</Label>
-            <Input
-              type="password"
-              name="password"
-              id="examplePassword"
-              placeholder="********"
-            />
-          </FormGroup>
-          <Button className="w-100">Submit</Button>
-        </Form>
-      </div>
-    </div>
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
   );
-};
+}
 
-export default Login;
+const theme = createTheme();
+
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="login-page">
+        <Container
+          sx={{
+            backgroundColor: "#53a881",
+          }}
+          component="main"
+          maxWidth="xs"
+          fixed
+        >
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 18,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="secondary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+      </div>
+    </ThemeProvider>
+  );
+}
